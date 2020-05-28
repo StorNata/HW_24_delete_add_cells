99f9b8f479dea0dc547dd4156527cdcdea203564
Storozheva Nataliia <storozheva@i.ua>
2020-05-28 17:19:53 +0300

    modified project

--- HW_24_delete_add_cells/Controllers/AddDeleteCellController.swift
+++ HW_24_delete_add_cells/Controllers/AddDeleteCellController.swift
@@ -8,134 +8,110 @@
 
 import UIKit
 
-class AddDeleteCellController: TableViewController {
+class AddDeleteCellController: ViewController, UITableViewDataSource, UITableViewDelegate {
     
-    private let initDataSourceForFirstSection = [Int.random(in: 0...100),
-    Int.random(in: 0...100),
-    Int.random(in: 0...100)]
+    @IBOutlet var tableView: UITableView?
+    private var items: [(name: String, source: [Int])] = []
     
-    private let initDataSourceForSecondSection = [Int.random(in: 0...100),
-       Int.random(in: 0...100),
-       Int.random(in: 0...100),
-       Int.random(in: 0...100)]
+    let dataSourceForFirstSection = [Int.random(in: 0...100),
+                                     Int.random(in: 0...100),
+                                     Int.random(in: 0...100)]
     
-    let indexPathSection = IndexPath()
+    let dataSourceForSecondSection = [Int.random(in: 0...100),
+                                      Int.random(in: 0...100),
+                                      Int.random(in: 0...100),
+                                      Int.random(in: 0...100)]
     
     override func setup() {
         super.setup()
         title = "HW_24 Add end Delete cell"
+        setupItems()
     }
-
+    
+    private func setupItems() {
+        items.removeAll()
+        items.append((name: "Section 1", source: dataSourceForFirstSection))
+        items.append((name: "Section 2", source: dataSourceForSecondSection))
+    }
     override func viewDidLoad() {
-            super.viewDidLoad()
-       
+        super.viewDidLoad()
         
-        dataSource.append(contentsOf: initDataSourceForFirstSection)
-//
-//            dataSource.append(contentsOf: initDataSourceForSecondSection)
-       
-            self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonClicked))
-            
-            let refreshControll = UIRefreshControl()
-            refreshControll.addTarget(self, action: #selector(renewData), for: UIControl.Event.valueChanged)
-            tableView?.refreshControl = refreshControll
-            
-        }
-
-        //MARK: - Refresh Data
-        @objc private func renewData() {
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
-                self.dataSource.removeAll()
-                
-//                       if self.indexPathSection.section == 0 {
-//                         self.dataSource.append(contentsOf: self.initDataSourceForFirstSection)
-//               } else {
-//             self.dataSource.append(contentsOf: self.initDataSourceForSecondSection)
-//                }
-                self.tableView?.reloadData()
-                self.tableView?.refreshControl?.endRefreshing()
-  
-            }
-        }
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonClicked))
         
-        //MARK: - Actions
-        @objc private func addButtonClicked() {
-   
-           
-            let index = 0
-            let insertIndex = index <= dataSource.count ? index : dataSource.count
-            
-            dataSource.insert(Int.random(in: 0...100), at: insertIndex)
-            
-            let indexPath = IndexPath(row: insertIndex, section: 0)
-            tableView?.insertRows(at: [indexPath], with: .automatic)
-            
+        let refreshControll = UIRefreshControl()
+        refreshControll.addTarget(self, action: #selector(renewData), for: UIControl.Event.valueChanged)
+        tableView?.refreshControl = refreshControll
+    }
+    
+    //MARK: - Refresh Data
+    @objc private func renewData() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) { [weak self] in
+            self?.setupItems()
+            self?.tableView?.reloadData()
+            self?.tableView?.refreshControl?.endRefreshing()
         }
-   
-        //MARK: - UITableViewDataSource
+    }
+    
+    //MARK: - Actions add
+    @objc private func addButtonClicked() {
+        
+        let row = 0
+        let section = 0
+        
+        items[section].source.insert(Int.random(in: 0...100), at: row)
+        items[section].source.insert(Int.random(in: 0...100), at: row + 1)
+        
+        let indexPath1 = IndexPath(row: row, section: section)
+        let indexPath2 = IndexPath(row: row + 1, section: section)
+        tableView?.insertRows(at: [indexPath1, indexPath2], with: .automatic)
+    }
+    //MARK: - Actions delete
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let row = 0
+        let section = 1
+        
+        if editingStyle == .delete && indexPath.section == section {
+            
+            items[section].source.remove(at: row)
+            
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    //MARK: - UITableViewDataSource
     
     func numberOfSections(in tableView: UITableView) -> Int {
-       return 2
-
-     }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var result = 0
+        return items.count
         
-        switch section {
-        case 0:
-            result = dataSource.count
-      
-       
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        return items[section].source.count
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cellIdentifier = ""
+        switch indexPath.section {
+        case 1:
+            cellIdentifier = "cell_id_1"
         default:
-            result = dataSource.count
-            
+            cellIdentifier = "cell_id"
         }
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         
-        return result
+        cell.textLabel?.text = String(items[indexPath.section].source[indexPath.row])
+        
+        return cell
     }
-        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-            var cellIdentifier = ""
-                   switch indexPath.section {
-                   case 1:
-                       cellIdentifier = "cell_id_1"
-                   default:
-                       cellIdentifier = "cell_id"
-                   }
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
     
-            
-            if indexPath.section == 0 {
-               
-                if let number = dataSource[indexPath.row] as? Int {
-                                     cell.textLabel?.text = String(number)
-                            }
-            } else {
-               
-                if let number = dataSource[indexPath.row] as? Int {
-                                                   cell.textLabel?.text = String(number)
-            }
-            }
-         
-            return cell
-        }
-        
-        func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-            return true
-        }
-        
-        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            if editingStyle == .delete {
-                dataSource.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .automatic)
-            }
-        }
- 
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        var result: String?
-        result = "секция No  \(section + 1)"
-        return result
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
- 
-   }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return items[section].name
+    }
+}
 
